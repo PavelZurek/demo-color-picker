09161433c838a91ec0c55e8d533736e2d94a9c58
Pavel Žůrek <PavelZrk@gmail.com>
2022-10-17 09:44:29 +0200

    Setup MUI

---
+++ pages/_app.tsx
@@ -0,0 +1,25 @@
+import {AppProps} from 'next/app';
+import {ThemeProvider} from '@mui/material/styles';
+import theme from '../src/theme';
+import Head from 'next/head';
+import {CssBaseline} from '@mui/material';
+import Layout from '../src/components/Layout';
+
+export default function MyApp(props: AppProps) {
+    const {Component, pageProps} = props;
+
+    return (
+        <>
+            <Head>
+                <title>Color picker app</title>
+                <meta name="viewport" content="initial-scale=1, width=device-width"/>
+            </Head>
+            <ThemeProvider theme={theme}>
+                <CssBaseline/>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </ThemeProvider>
+        </>
+    )
+}
---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import {FC} from 'react';
+import {Container} from '@mui/material';
+
+const Layout: FC = ({children}) => {
+    return (
+        <Container maxWidth="lg">
+            {children}
+        </Container>
+    )
+}
+
+export default Layout
